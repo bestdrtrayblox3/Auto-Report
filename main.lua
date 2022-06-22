8cc6665be0bf1a383c4637972a94d4751760ec44
snnwer <75314624+snnwer@users.noreply.github.com>
2022-06-23 03:44:31 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -23,11 +23,13 @@ local Default = {
 	    Blacklist = "https://raw.githubusercontent.com/CF-Trail/Auto-Report/main/words/blacklisted.lua";
 	    Whitelist = "https://raw.githubusercontent.com/CF-Trail/Auto-Report/main/words/whitelisted.lua";
 	};
+	
+	isExecuted = true;
 }
 
 if not autoreport then
 	getgenv().autoreport = Default
-else
+elseif autoreport.isExecuted
 	return warn("Auto-Report is already executed!")
 end;
 
